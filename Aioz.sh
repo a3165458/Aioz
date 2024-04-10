8bc57a118453b07132e7eee59c9e1024bb463a4b
smeb y <48400087+a3165458@users.noreply.github.com>
2024-04-10 16:53:45 +0800

    Update Aioz.sh

--- Aioz.sh
+++ Aioz.sh
@@ -49,6 +49,18 @@ echo "后续使用screen -r aioznode查看运行情况。"
 
 }
 
+
+function check_status() {
+./aioznode stats
+
+}
+
+function reward balance() {
+./aioznode reward balance
+
+}
+
+
 # 主菜单
 function main_menu() {
     while true; do
@@ -60,11 +72,15 @@ function main_menu() {
         echo "退出脚本，请按键盘ctrl c退出即可"
         echo "请选择要执行的操作:"
         echo "1. 安装节点"
-        read -p "请输入选项（1-12）: " OPTION
+        echo "2. 查看节点状态"
+        echo "3. 查看收益"
+        read -p "请输入选项（1-3）: " OPTION
 
         case $OPTION in
         1) install_node ;;
-        *) echo "无效选项。" ;;
+        2) check_status ;;
+        3) reward balance ;;
+        *) e) check_status ;;cho "无效选项。" ;;
         esac
         echo "按任意键返回主菜单..."
         read -n 1
